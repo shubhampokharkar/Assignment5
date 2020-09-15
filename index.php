39f2ad6f492b30c3312f68a9b4b1815f1d3237f9
shubhampokharkar <67820083+shubhampokharkar@users.noreply.github.com>
2020-09-16 00:07:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  <head>
  <title>Firodiya Dance Competetion</title>
  <link rel="stylesheet" type="text/css" href="dancestyle1.css">
+<script src="jquery-3.5.1.min.js"></script>
  </head>
  <body >
  <div class="main">
